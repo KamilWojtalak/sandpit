14533aed8ca25bf4fc734215a92296fcb78f3f74
KamilWojtalak <k.wojtalak@ideo.pl>
2022-09-05 21:23:57 +0200

    [vue] events

--- vue/Vue - The Complete Guide/basics-03-events-starting-code/app.js
+++ vue/Vue - The Complete Guide/basics-03-events-starting-code/app.js
@@ -2,14 +2,18 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: ''
     };
   },
   methods: {
-    add() {
-      return this.counter++;
+    add(num) {
+      this.counter += num;
     },
-    reduce() {
-      return this.counter--;
+    reduce(num) {
+      this.counter -= num;
+    },
+    setName(e) {
+      this.name = e.target.value;
     }
   }
 });
