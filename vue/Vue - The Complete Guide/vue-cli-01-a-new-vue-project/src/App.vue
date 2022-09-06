5979a578d6912b74a18ec6a36b34fdcc4222c86c
KamilWojtalak <k.wojtalak@ideo.pl>
2022-09-06 14:43:01 +0200

    [vue] first vue component

---
+++ vue/Vue - The Complete Guide/vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,33 @@
+<template lang="">
+    <section>
+        <h2>My friends</h2>
+        <ul>
+            <li>test</li>
+        </ul>
+    </section>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            friends: [
+                {
+                    id: 'manuel',
+                    name: 'Manuel Lorenz',
+                    phone: '123 123 123',
+                    email: 'manuel@localhost.com',
+                },
+                {
+                    id: 'julie',
+                    name: 'Julie Jones',
+                    phone: '121 121 121',
+                    email: 'julie@localhost.com',
+                },
+            ]
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
