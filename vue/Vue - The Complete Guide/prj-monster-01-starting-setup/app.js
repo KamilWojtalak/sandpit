dc84343200234afb025e9ceb89cc9bae3b227d32
KamilWojtalak <k.wojtalak@ideo.pl>
2022-09-06 11:52:08 +0200

    [vue] add listeners and update health bar

--- vue/Vue - The Complete Guide/prj-monster-01-starting-setup/app.js
+++ vue/Vue - The Complete Guide/prj-monster-01-starting-setup/app.js
@@ -1,23 +1,31 @@
 function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
 const app = Vue.createApp({
-    data() {
-        return {
-            playerHealth: 100,
-            monsterHealth: 100,
-        }
+  data() {
+    return {
+      playerHealth: 100,
+      monsterHealth: 100,
+    };
+  },
+  computed: {
+    monsterBarStyles() {
+        return {width: this.monsterHealth + '%'}
     },
-    methods: {
-        attackMonster() {
-            this.monsterHealth -= getRandomNumber(5, 12);
-            this.attackPlayer();
-        },
-        attackPlayer() {
-            this.playerHealth -= getRandomNumber(7, 13);
-        }
-    }
+    playerBarStyles() {
+        return {width: this.playerHealth + '%'}
+    },
+  },
+  methods: {
+    attackMonster() {
+      this.monsterHealth -= getRandomNumber(5, 12);
+      this.attackPlayer();
+    },
+    attackPlayer() {
+      this.playerHealth -= getRandomNumber(7, 13);
+    },
+  },
 });
 
 app.mount("#game");
